47468fd4b88fff3f1c0c880af6baa9e4cb4f50eb
iMoein <moeinghezelbash@gmail.com>
2026-06-13 14:34:40 +0330

    Update

---
+++ index.php
@@ -0,0 +1,196 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Title -->
+  <title>Iran Eco Trip | Iran Travel and Eco Tour | IranEcoTrip.com</title>
+
+  <!-- Required Meta Tags Always Come First -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Favicon -->
+
+  <!-- CSS Global Compulsory -->
+  <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.css">
+
+  <!-- CSS Implementing Plugins -->
+  <link rel="stylesheet" href="../assets/vendor/font-awesome/css/all.min.css">
+  <link rel="stylesheet" href="../assets/vendor/hs-megamenu/src/hs.megamenu.css">
+  <link rel="stylesheet" href="../assets/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css">
+  <link rel="stylesheet" href="../assets/vendor/custombox/dist/custombox.min.css">
+  <link rel="stylesheet" href="../assets/vendor/animate.css/animate.min.css">
+  <link rel="stylesheet" href="../assets/vendor/slick-carousel/slick/slick.css">
+  <link rel="stylesheet" href="../assets/vendor/cubeportfolio/css/cubeportfolio.min.css">
+  <link rel="stylesheet" href="../assets/vendor/fancybox/jquery.fancybox.css">
+  <link rel="stylesheet" href="../assets/vendor/dzsparallaxer/dzsparallaxer.css">
+
+  <!-- CSS Space Template -->
+  <link rel="stylesheet" href="../assets/css/theme.css">
+</head>
+
+<body>
+
+
+  <!-- ========== MAIN CONTENT ========== -->
+  <main id="content" class=" direction-ltr text-right">
+    <!-- Hero Section -->
+    <div class="d-lg-flex align-items-lg-center height-100vh--lg">
+      <div class="container space-3-top space-2-bottom space-0--lg">
+        <div class="row justify-content-lg-between align-items-lg-center">
+          <div class="col-lg-6 mb-9 mb-lg-0">
+            <img class="w-100" src="../../assets/svg/2505.svg" alt="Image Description">
+          </div>
+
+          <div class="col-lg-5">
+            <!-- Title -->
+            <div class="mb-4">
+              <h1 class="h2">Iran Eco Trip</h1>
+              <p class="lead">Ever wondered about visiting Iran? Come along to travel, discover Iran a land of four seasons, history and culture, souvenir and authenticity. This is not a tourism slogan, this is the reality inferred from the experience of visitors who have been impressed by Iran’s beauties and amazing attractions. Antiquity and richness of its culture and civilization, the variety of natural and geographical attractions, four - season climate, diverse cultural sites in addition to different tribes with different and fascinating traditions and customs have made Iran as a treasury of tangible and intangible heritage.</p>
+            </div>
+            <!-- End Title -->
+            <a class="btn btn-primary" href="https://www.instagram.com/iranecotrip.ir/?hl=en">Go to Instagram</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Hero Section -->
+  </main>
+  <!-- ========== END MAIN CONTENT ========== -->
+
+  <!-- ========== FOOTER ========== -->
+  <footer class="position-absolute-bottom-0--lg">
+    <div class="container text-center space-1">
+      <p class="small mb-0">&copy; IranEcoTrip.com</p>
+    </div>
+  </footer>
+  <!-- ========== END FOOTER ========== -->
+
+
+  <!-- ========== END SECONDARY CONTENTS ========== -->
+    <!-- Go to Top -->
+  <a class="js-go-to u-go-to" href="javascript:;"
+    data-position='{"bottom": 15, "right": 15 }'
+    data-type="fixed"
+    data-offset-top="400"
+    data-compensation="#header"
+    data-show-effect="slideInUp"
+    data-hide-effect="slideOutDown">
+    <span class="fa fa-arrow-up u-go-to__inner"></span>
+  </a>
+  <!-- End Go to Top -->    <!-- JS Global Compulsory -->
+  <script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
+  <script src="../assets/vendor/jquery-migrate/dist/jquery-migrate.min.js"></script>
+  <script src="../assets/vendor/popper.js/dist/umd/popper.min.js"></script>
+  <script src="../assets/vendor/bootstrap/bootstrap.min.js"></script>    <!-- JS Implementing Plugins -->
+  <script src="../assets/vendor/hs-megamenu/src/hs.megamenu.js"></script>
+  <script src="../assets/vendor/jquery-validation/dist/jquery.validate.min.js"></script>
+  <script src="../assets/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js"></script>
+  <script src="../assets/vendor/custombox/dist/custombox.min.js"></script>
+  <script src="../assets/vendor/custombox/dist/custombox.legacy.min.js"></script>
+  <script src="../assets/vendor/instafeed.js/instafeed.min.js"></script>
+  <script src="../assets/vendor/dzsparallaxer/dzsparallaxer.js"></script>  
+  <script src="../assets/vendor/cubeportfolio/js/jquery.cubeportfolio.min.js"></script>
+  <script src="../assets/vendor/fancybox/jquery.fancybox.min.js"></script>
+  <script src="../assets/vendor/slick-carousel/slick/slick.js"></script>    <!-- JS Space -->
+  <script src="../assets/js/hs.core.js"></script>
+  <script src="../assets/js/components/hs.header.js"></script>
+  <script src="../assets/js/components/hs.unfold.js"></script>
+  <script src="../assets/js/components/hs.validation.js"></script>
+  <script src="../assets/js/helpers/hs.focus-state.js"></script>
+  <script src="../assets/js/components/hs.malihu-scrollbar.js"></script>
+  <script src="../assets/js/components/hs.modal-window.js"></script>
+  <script src="../assets/js/components/hs.show-animation.js"></script>
+  <script src="../assets/js/components/hs.instagram.js"></script>
+  <script src="../assets/js/components/hs.cubeportfolio.js"></script>
+  <script src="../assets/js/components/hs.sticky-block.js"></script>
+  <script src="../assets/js/components/hs.fancybox.js"></script>
+  <script src="../assets/js/components/hs.go-to.js"></script>
+  <script src="../assets/js/components/hs.slick-carousel.js"></script>
+    <!-- JS Plugins Init. -->
+  <script>
+    $(window).on('load', function () {
+      // initialization of HSMegaMenu component
+      $('.js-mega-menu').HSMegaMenu({
+        event: 'hover',
+        pageContainer: $('.container'),
+        breakpoint: 991,
+        hideTimeOut: 0
+      });
+    });
+		
+	$(document).on('ready', function () {
+		// initialization of slick carousel
+		$.HSCore.components.HSSlickCarousel.init('.js-slick-carousel');
+	  });
+  
+    $(document).on('ready', function () {
+      // initialization of header
+      $.HSCore.components.HSHeader.init($('#header'));
+
+      // initialization of unfold component
+      $.HSCore.components.HSUnfold.init($('[data-unfold-target]'), {
+        afterOpen: function () {
+          if (!$('body').hasClass('IE11')) {
+            $(this).find('input[type="search"]').focus();
+          }
+        }
+      });
+
+      // initialization of form validation
+      $.HSCore.components.HSValidation.init('.js-validate', {
+        rules: {
+          confirmPassword: {
+            equalTo: '#password'
+          }
+        }
+      });
+
+      // initialization of forms
+      $.HSCore.helpers.HSFocusState.init();
+
+      // initialization of malihu scrollbar
+      $.HSCore.components.HSMalihuScrollBar.init($('.js-scrollbar'));
+
+      // initialization of autonomous popups
+      $.HSCore.components.HSModalWindow.init('[data-modal-target]', '.js-signup-modal', {
+        autonomous: true
+      });
+
+      // initialization of show animations
+      $.HSCore.components.HSShowAnimation.init('.js-animation-link');
+
+      // initialization of instagram api
+      $.HSCore.components.HSInstagram.init('#instaFeed', {
+        resolution: 'standard_resolution',
+        after: function () {
+          // initialization of masonry.js
+          var $grid = $('.js-instagram').masonry({
+            percentPosition: true
+          });
+
+          // initialization of images loaded
+          $grid.imagesLoaded().progress(function () {
+            $grid.masonry();
+          });
+        }
+      });
+
+      // initialization of cubeportfolio
+      $.HSCore.components.HSCubeportfolio.init('.cbp');
+
+      // initialization of sticky blocks
+      setTimeout(function () {
+        $.HSCore.components.HSStickyBlock.init('.js-sticky-block');
+      }, 300);
+
+      // initialization of fancybox
+      $.HSCore.components.HSFancyBox.init('.js-fancybox');
+
+      // initialization of go to
+      $.HSCore.components.HSGoTo.init('.js-go-to');
+    });
+  </script>
+  
+</body>
+</html>
